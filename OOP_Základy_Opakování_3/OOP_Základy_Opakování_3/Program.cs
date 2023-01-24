9489b42891cd2f12b09ce74340d544b7f13ea6cc
Mawi111 <86527443+Mawi111@users.noreply.github.com>
2023-01-24 23:36:37 +0100

    Raketa

--- OOP_Základy_Opakování_3/OOP_Základy_Opakování_3/Program.cs
+++ OOP_Základy_Opakování_3/OOP_Základy_Opakování_3/Program.cs
@@ -13,36 +13,53 @@ namespace OOP_Základy_Opakování_3
         {
             Planeta Proxima = new Planeta("Proxima Centauri b", 4.2F, "Kentaur");
             Planeta Luyten = new Planeta("Luyten b", 12.36F, "Malý pes");
-            Planeta Wolf = new Planeta("Wolf 1061c", 13.8F, "Hadonoš"); 
+            Planeta Wolf = new Planeta("Wolf 1061c", 13.8F, "Hadonoš");
             Zeme zeme = new Zeme();
-            Console.WriteLine("Výchozí bod: " + zeme.vratVychoziPlaneta());
-            Console.WriteLine("Soustava: "+zeme.vratVychoziSoustavu());
+            Raketa raketa = new Raketa();
+
             Console.WriteLine("==============================");
-            Console.WriteLine("Planety: \n Proxima Centauri b \n Luyten b \n Wolf 1061c \n\nVyberte cílovou planetu:");
-            string cil = Console.ReadLine();
-            Console.WriteLine("==============================");
-            if (cil == "Proxima Centauri b")
+
+            Console.WriteLine("Stav paliva: " + raketa.stavPaliva + " hl");
+            Console.WriteLine("Aktuální stav rakety: "+raketa.stavLodi1);
+
+            Console.WriteLine("Zmáčkni enter");
+            string pokracovat = Console.ReadKey().Key.ToString();
+            if (pokracovat == "Enter")
             {
-                Console.WriteLine("Cílová planeta: " + Proxima.vratNazev());
-                Console.WriteLine("Vzdálenost od Země: " + Proxima.vratPozice() + " světelných let");
-                Console.WriteLine("Soustava: " + Proxima.vratSoustava());
-            }
-            else if (cil == "Luyten b")
-            {
-                Console.WriteLine("Cílová planeta: " + Luyten.vratNazev());
-                Console.WriteLine("Vzdálenost od Země: " + Luyten.vratPozice() + " světelných let");
-                Console.WriteLine("Soustava: " + Luyten.vratSoustava());
-            }
-            else if (cil == "Wolf 1061c")
-            {
-                Console.WriteLine("Cílová planeta: " + Wolf.vratNazev());
-                Console.WriteLine("Vzdálenost od Země: " + Wolf.vratPozice() + " světelných let");
-                Console.WriteLine("Soustava: " + Wolf.vratSoustava());
+                Console.WriteLine("==============================");
+
+                Console.WriteLine("Výchozí bod: " + zeme.vratVychoziPlaneta());
+                Console.WriteLine("Soustava: " + zeme.vratVychoziSoustavu());
+                Console.WriteLine("==============================");
+                Console.WriteLine("Planety: \n Proxima Centauri b \n Luyten b \n Wolf 1061c \n\nVyberte cílovou planetu:");
+                string cil = Console.ReadLine();
+                Console.WriteLine("==============================");
+                if (cil == "Proxima Centauri b")
+                {
+                    Console.WriteLine("Cílová planeta: " + Proxima.vratNazev());
+                    Console.WriteLine("Vzdálenost od Země: " + Proxima.vratPozice() + " světelných let");
+                    Console.WriteLine("Soustava: " + Proxima.vratSoustava());
+                }
+                else if (cil == "Luyten b")
+                {
+                    Console.WriteLine("Cílová planeta: " + Luyten.vratNazev());
+                    Console.WriteLine("Vzdálenost od Země: " + Luyten.vratPozice() + " světelných let");
+                    Console.WriteLine("Soustava: " + Luyten.vratSoustava());
+                }
+                else if (cil == "Wolf 1061c")
+                {
+                    Console.WriteLine("Cílová planeta: " + Wolf.vratNazev());
+                    Console.WriteLine("Vzdálenost od Země: " + Wolf.vratPozice() + " světelných let");
+                    Console.WriteLine("Soustava: " + Wolf.vratSoustava());
+                }
+                else
+                {
+                    Console.WriteLine("Planeta neni v 'databázi'.");
+                }
             }
             else
-            {
-                Console.WriteLine("Planeta neni v 'databázi'.");
-            }
+                Console.WriteLine("Nezmáčknul jsi enter");
+            
             Console.ReadKey();
         }
     }
--- OOP_Základy_Opakování_3/OOP_Základy_Opakování_3/Raketa.cs
+++ OOP_Základy_Opakování_3/OOP_Základy_Opakování_3/Raketa.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +9,25 @@ namespace OOP_Základy_Opakování_3
 {
     internal class Raketa
     {
-        private int pozice;
-        public int posadka;
+        public int stavPaliva;
+        public string stavLodi1;
+        public string stavLodi2;
+        public Random Random;
 
+        public Raketa()
+        {
+            stavPaliva = 30000;
+            stavLodi1 = "Dobrý";
+            stavLodi2 = "Špatný";
 
-        
-        
-
-        
+        }
+        public int VratStavPaliva()
+        {
+            return stavPaliva;
+        }
+        public string VratStavLodi()
+        {
+            return stavLodi1;
+        }
     }
 }
