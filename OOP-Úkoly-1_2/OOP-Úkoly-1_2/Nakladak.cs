cb726e080e3725b375573abc238dcfa083248d5e
Mawi111 <86527443+Mawi111@users.noreply.github.com>
2023-01-29 19:14:14 +0100

    Nakladak

---
+++ OOP-Úkoly-1_2/OOP-Úkoly-1_2/Nakladak.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Úkoly_1_2
+{
+    internal class Nakladak
+    {
+        public int naklad;
+        public int nostnost;
+        public int misto;
+
+        public Nakladak()
+        {
+            nostnost = 3000;
+            naklad = 0;
+        }
+        public int Nalozeni1()
+        {
+            naklad = naklad + 10000;
+            if (naklad > nostnost)
+            {
+                Console.WriteLine("Nelze naložit. Maximální nostnost je 3 tuny");
+            }
+            return naklad*0;
+            
+        }
+        public int Nalozeni2()
+        {
+            naklad = 0 + 500;
+            if (naklad > nostnost)
+            {
+                Console.WriteLine("Nelze naložit. Maximální nostnost je 3 tuny");
+            }
+            else
+            {
+                Console.WriteLine("Lze naložit "+naklad+" kg");
+            }
+             return naklad;
+        }
+        public int Vylozeni1()
+        {
+            naklad = naklad - 300;
+            Console.WriteLine("Aktuální náklad: "+naklad);
+            naklad = misto;
+            return naklad;
+        }
+        //public int Vylozeni2()//Musím se zeptat učitele
+        //{
+        //    misto = naklad;
+        //    naklad = naklad - 1000;
+        //    if (naklad < misto)
+        //    {
+        //        Console.WriteLine("Nemůžu vyložit 1 tunu, když mám na korbě "+misto);
+        //    }
+        //    return naklad;
+        //}
+
+
+    }
+}
---
+++ OOP-Úkoly-1_2/OOP-Úkoly-1_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Úkoly_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nakladak nakladak = new Nakladak();
+
+            Console.WriteLine("Probíhá první naloženi 10 tun...[ENTER]");
+            Console.ReadKey();
+            nakladak.Nalozeni1();
+            Console.WriteLine("Probíhá druhé naloženi 500 kg...[ENTER]");
+            Console.ReadKey();
+            nakladak.Nalozeni2();
+            Console.WriteLine("Probíhá první vyloženi 300 kg...[ENTER]");
+            Console.ReadKey();
+            nakladak.Vylozeni1();
+            Console.WriteLine("Probíhá druhé vyloženi 1 tuny ...[ENTER]");
+            Console.ReadKey();
+            nakladak.Vylozeni2();
+
+            Console.ReadKey();
+        }
+    }
+}
