17145d8998dbd668edab9a1a3f26333a74a83799
Mawi111 <86527443+Mawi111@users.noreply.github.com>
2023-01-29 20:12:32 +0100

    Představeni dvou lidi

---
+++ OOP-Úkoly-1-3/OOP-Úkoly-1-3/Clovek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Úkoly_1_3
+{
+    internal class Clovek
+    {
+        public string jmeno;
+        public int vek;
+
+        public Clovek(string jmeno, int vek)
+        {
+            this.jmeno = jmeno; // Proč je to takhel a co to přesně dělá 
+            this.vek = vek;
+        }
+    }
+}
---
+++ OOP-Úkoly-1-3/OOP-Úkoly-1-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Úkoly_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Clovek clovek = new Clovek("Karel Novák", 33); // Proč je to takhel a co to přesně dělá 
+            Clovek kamarad = new Clovek("Cyril Nový", 27);
+            
+            
+            
+           
+            Console.WriteLine("Ahoj já jsem "+clovek.jmeno+" je mi "+clovek.vek+" let a můj kamarád je "+kamarad.jmeno);
+            Console.WriteLine("Ahoj já jsem " + kamarad.jmeno + " je mi " + kamarad.vek + " let a můj kamarád je " + clovek.jmeno);
+
+            Console.ReadKey();
+        }
+    }
+}
