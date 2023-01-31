ef90138269ae23bd356ace2ba0e897be3424b3c5
Mawi111 <86527443+Mawi111@users.noreply.github.com>
2023-01-31 22:44:38 +0100

    Nakladak-interaktivni

---
+++ OOP-Nakladak-Interaktivni/OOP-Nakladak-Interaktivni/Nakladak.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Nakladak_Interaktivni
+{
+    internal class Nakladak
+    {
+        public int naklad;
+        private int misto;
+        public int nostnost;
+        public int vyklad;
+
+        public Nakladak()
+        {
+            nostnost = 3000;
+        }
+        public int Nalozeni()
+        {
+            Console.WriteLine("Kolik chceš naložit? (kg)");
+            naklad = int.Parse(Console.ReadLine());    
+            if (naklad > nostnost)
+            {
+                Console.WriteLine("Nelze naložit. Maximální nostnost je 3 tuny...[ENTER]");
+                Console.ReadKey();
+                Environment.Exit(3);
+            }
+            else
+            {
+                misto = nostnost - naklad;
+                Console.WriteLine("Probíhá naložení " +  naklad + " kilogramů. Aktuální místo je " + misto + " kilogramů...[ENTER]");
+            }
+            Console.ReadKey();
+            return naklad;
+            
+        }
+        public int Vylozeni()
+        {
+            Console.WriteLine("Kolik chceš vyložit? (kg)");
+            vyklad = int.Parse(Console.ReadLine());
+            if (vyklad > naklad) 
+            {
+                Console.WriteLine("Nelze vyložit...");
+                Console.ReadKey();
+                Environment.Exit(3);
+
+            }
+            else
+            {
+                naklad = naklad - vyklad;
+                misto = misto - vyklad;
+                Console.WriteLine("Probíhá vyložení " + vyklad + " kg aktuálně je naloženo " + naklad + " kg. Misto na korbě je " + misto + "...[ENTER]");
+                
+            }
+            return naklad;
+        }
+    }
+}
---
+++ OOP-Nakladak-Interaktivni/OOP-Nakladak-Interaktivni/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Nakladak_Interaktivni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Nakladak nakladak = new Nakladak();
+            nakladak.Nalozeni();
+            nakladak.Vylozeni();
+            Console.ReadKey();
+        }
+    }
+}
