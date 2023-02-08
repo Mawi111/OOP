ed360afdeb9aa63147941a96a0321045477f0b2b
Mawi111 <86527443+Mawi111@users.noreply.github.com>
2023-02-08 07:58:51 +0100

    Reseni

--- OOP-Ukoly-2-2/OOP-Ukoly-2-2/GeneratorSlov.cs
+++ OOP-Ukoly-2-2/OOP-Ukoly-2-2/GeneratorSlov.cs
@@ -42,5 +42,24 @@ namespace OOP_Ukoly_2_2
             return String.Format("{0} ", podmety[3] , privlastky[4]);
 
         }
+
+        //private string[] podmet = new string[] { "jednorožec" , "programátor" , "manažer", "hroch" , "T-rex"}; // Využití polí kdž vím že toho bude vždy stějne jinak spíš využívám seznam nebo slovník
+        //private string[] prisudek = new string[] { "spal", "letěl", "vařil", "uklízel", "derivoval" };
+        //private string[] privlastek = new string[] { "modrý", "vlhký", "hubený", "chytrý", "automatizovaný" };
+        //private string[] prislovce = new string[] { "rychle", "s oblibou", "hodně", "málo", "se zpoždením" };
+        //private string[] pum = new string[] { "pod stolem", "v lese", "u babičky", "na stole", "v práci" };
+        //Random random = new Random();
+
+        //public string GenerujVetu()
+        //{
+        //    string veta = "";
+        //    veta += privlastek[random.Next(0, privlastek.Length)]+ " ";
+        //    veta += podmet[random.Next(0, podmet.Length)]+ " ";
+        //    veta += prisudek[random.Next(0, prisudek.Length)] + " ";
+        //    veta += prislovce[random.Next(0, privlastek.Length)] + " ";
+        //    veta += pum[random.Next(0, pum.Length)] + ".";
+
+        //    return veta.Substring(0/*začne od nuly*/,1 /*vezme prvni znak*/).ToUpper()/*zvětší vybraný znak*/ + veta.Substring(1);
+        //}
     }
 }
